c021519222bc8d0b712aba499637badbfbb43129
zhanpeissov3 <zhanpeissov3@gmail.com>
2020-05-05 20:04:14 +0600

    first commit

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Service
 
-# Register your models here.
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'icon', 'price', 'content', 'photo', 'publish', 'created', 'updated']
+    list_filter = ['created', 'updated', 'price']
+    list_editable = ['price', ]
+    search_fields = ('title', 'content')
+
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Service, ServiceAdmin)
--- services/models.py
+++ services/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Service(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    icon = models.ImageField(upload_to='icons/%Y/%m/%d', blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    content = models.TextField()
+    photo = models.ImageField(upload_to='photo/%Y/%m/%d', blank=True)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Meta:
+    ordering = ('title',)
+    verbose_name = 'Услуга'
+    verbose_name_plural = 'Услуги'
+
+
+def __str__(self):
+    return self.title
